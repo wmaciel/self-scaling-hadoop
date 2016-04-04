4cf45a2bca85296b13df5dfc862b39714688758c
Walther Maciel <walthermaciel@gmail.com>
2016-04-04 15:38:59 -0700

    modifications to the load balancer

--- src/load_balancer.py
+++ src/load_balancer.py
@@ -5,54 +5,63 @@ import upsize
 import downsize
 import health_check
 
+INITIAL_PATIENCE = 50
+SLEEP = 10
+BIG_SLEEP = 120
 
-def get_queue_size():
-    # TODO:
-    return 0
+QUEUE_WEIGHT = 3
+MEMORY_WEIGHT = 2
+CPU_WEIGHT = 1
 
+HIGH_STATE_PERCENTAGE = 0.75
+LOW_STATE_PERCENTAGE = 0.25
 
-def get_cpu_load():
-    # TODO:
-    return 0
-
-
-def get_memory_load():
-    # TODO:
-    return 0
+THRESHOLD = 0.8
 
 
-def compute_cluster_load():
-    # TODO:
-    return 0
+def compute_cluster_load(w_queue, w_memory, w_cpu):
+    metrics = health_check.get_cluster_metrics()
+    queue_load = float(int(metrics['appsPending'])) / \
+                 sum([
+                     int(metrics['appsSubmitted']),
+                     int(metrics['appsPending']),
+                     int(metrics['appsRunning'])
+                 ])
+    memory_load = float(int(metrics['totalMB']) - int(metrics['availableMB'])) / int(metrics['totalMB'])
+    cpu_load = float(int(metrics['totalVirtualCores']) - int(metrics['availableVirtualCores'])) / int(metrics['totalVirtualCores'])
+
+    return sum([
+        w_queue * queue_load,
+        w_memory * memory_load,
+        w_cpu * cpu_load
+    ]) / sum([QUEUE_WEIGHT, MEMORY_WEIGHT, CPU_WEIGHT])
 
 
 def downsize_cluster():
     downsize.decommission(True)
     downsize.removeDecommissionedMachine()
-    
     return True
+
 
 def upsize_cluster():
     upsize.upsize()
-    
     return True
 
 
-def low_state(low_threshold):
-    patience = 50
-
+def low_state(low_threshold, patience):
     while compute_cluster_load() < low_threshold:
         patience -= 1
-        time.sleep(10)
+        time.sleep(SLEEP)
 
         if patience < 0:
             downsize_cluster()
-            time.sleep(120)
-            return
+            time.sleep(BIG_SLEEP)
+            break
+
+    return patience
 
 
-def high_state(threshold, high_threshold):
-    patience = 50
+def high_state(threshold, high_threshold, patience):
     load = compute_cluster_load()
 
     while load > high_threshold:
@@ -61,39 +70,42 @@ def high_state(threshold, high_threshold):
         else:
             patience -= 1
 
-        time.sleep(10)
+        time.sleep(SLEEP)
 
         if patience < 0:
             upsize_cluster()
-            time.sleep(120)
-            return
+            time.sleep(BIG_SLEEP)
+            break
+
+    return patience
 
 
 def main(threshold, w_queue, w_memory, w_cpu):
-    high_threshold = 0.75 * threshold
-    low_threshold = 0.25 * threshold
+    high_threshold = HIGH_STATE_PERCENTAGE * threshold
+    low_threshold = LOW_STATE_PERCENTAGE * threshold
+    patience = INITIAL_PATIENCE
+
+    last_state = None
 
     while True:
-        load = compute_cluster_load()
+        load = compute_cluster_load(w_queue, w_memory, w_cpu)
         if load < low_threshold:
-            low_state(low_threshold)
+            if last_state != 'low':
+                last_state = 'low'
+                patience = INITIAL_PATIENCE
+            patience = low_state(low_threshold, patience)
         elif load > high_threshold:
-            high_state(threshold, high_threshold)
+            if last_state != 'high':
+                last_state = 'high'
+                patience = INITIAL_PATIENCE
+            patience = high_state(threshold, high_threshold, patience)
+        else:
+            patience += 1
+            time.sleep(SLEEP)
 
-
+        if patience > INITIAL_PATIENCE or patience <= 0:
+            patience = INITIAL_PATIENCE
 
 
 if __name__ == '__main__':
-    # Threshold for adding new machine
-    threshold = 10
-
-    # Queue size weight
-    w_queue = 5
-
-    # Memory load weight
-    w_memory = 3
-
-    # CPU load weight
-    w_cpu = 1
-
-    main(threshold, w_queue, w_memory, w_cpu)
+    main(THRESHOLD, QUEUE_WEIGHT, MEMORY_WEIGHT, CPU_WEIGHT)
